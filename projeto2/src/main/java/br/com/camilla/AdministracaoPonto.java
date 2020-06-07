5245611268a886a305e70647c72344ada89392c0
Camilla-Marques <camilla.93marques@gmail.com>
2020-06-07 03:45:09 -0300

    Classe responsável pela administração do ponto

--- projeto2/src/main/java/br/com/camilla/AdministracaoPonto.java
+++ projeto2/src/main/java/br/com/camilla/AdministracaoPonto.java
@@ -1,8 +1,35 @@
 package br.com.camilla;
 
-import java.util.Date;
+import java.util.List;
 
-public class AdministracaoPonto {
+public class AdministracaoSistema {
+	
+	private List <RegistroPonto> registrosDePonto;
+	private List <Empregado> listaDeEmpregados;
+	/**
+	 * @return the registrosDePonto
+	 */
+	public List<RegistroPonto> getRegistrosDePonto() {
+		return registrosDePonto;
+	}
+	/**
+	 * @param registrosDePonto the registrosDePonto to set
+	 */
+	public void setRegistrosDePonto(List<RegistroPonto> registrosDePonto) {
+		this.registrosDePonto = registrosDePonto;
+	}
+	/**
+	 * @return the listaDeEmpregados
+	 */
+	public List<Empregado> getListaDeEmpregados() {
+		return listaDeEmpregados;
+	}
+	/**
+	 * @param listaDeEmpregados the listaDeEmpregados to set
+	 */
+	public void setListaDeEmpregados(List<Empregado> listaDeEmpregados) {
+		this.listaDeEmpregados = listaDeEmpregados;
+	}
 	
 	
 }
