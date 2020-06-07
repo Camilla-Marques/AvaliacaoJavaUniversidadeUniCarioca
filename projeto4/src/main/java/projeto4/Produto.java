e6f49abdab4077049fb74a2a83353af2550bdc7b
Camilla-Marques <camilla.93marques@gmail.com>
2020-06-07 04:49:57 -0300

    Classe responsável pelos produtos

---
+++ projeto4/src/main/java/projeto4/Produto.java
@@ -0,0 +1,58 @@
+package projeto4;
+
+public class Produto {
+	
+	private Long idProduto;
+	private String linhaProduto;
+	private String nomeProduto;
+	private String descricaoProduto;
+	/**
+	 * @return the idProduto
+	 */
+	public Long getIdProduto() {
+		return idProduto;
+	}
+	/**
+	 * @param idProduto the idProduto to set
+	 */
+	public void setIdProduto(Long idProduto) {
+		this.idProduto = idProduto;
+	}
+	/**
+	 * @return the linhaProduto
+	 */
+	public String getLinhaProduto() {
+		return linhaProduto;
+	}
+	/**
+	 * @param linhaProduto the linhaProduto to set
+	 */
+	public void setLinhaProduto(String linhaProduto) {
+		this.linhaProduto = linhaProduto;
+	}
+	/**
+	 * @return the nomeProduto
+	 */
+	public String getNomeProduto() {
+		return nomeProduto;
+	}
+	/**
+	 * @param nomeProduto the nomeProduto to set
+	 */
+	public void setNomeProduto(String nomeProduto) {
+		this.nomeProduto = nomeProduto;
+	}
+	/**
+	 * @return the descricaoProduto
+	 */
+	public String getDescricaoProduto() {
+		return descricaoProduto;
+	}
+	/**
+	 * @param descricaoProduto the descricaoProduto to set
+	 */
+	public void setDescricaoProduto(String descricaoProduto) {
+		this.descricaoProduto = descricaoProduto;
+	}
+
+}
