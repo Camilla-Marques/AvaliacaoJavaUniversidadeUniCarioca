98e22efaaa6936e02cdcc0d3a644f9a04f661dcc
Camilla-Marques <camilla.93marques@gmail.com>
2020-06-07 04:46:28 -0300

    Classe responsável pelos dados do cliente pessoa física

---
+++ projeto4/src/main/java/projeto4/ClientePF.java
@@ -0,0 +1,45 @@
+package projeto4;
+
+public class ClientePF extends Cliente {
+	
+	private String numeroCPF;
+	private String nomeGestor;
+	private String telefoneGestor;
+	/**
+	 * @return the numeroCPF
+	 */
+	public String getNumeroCPF() {
+		return numeroCPF;
+	}
+	/**
+	 * @param numeroCPF the numeroCPF to set
+	 */
+	public void setNumeroCPF(String numeroCPF) {
+		this.numeroCPF = numeroCPF;
+	}
+	/**
+	 * @return the nomeGestor
+	 */
+	public String getNomeGestor() {
+		return nomeGestor;
+	}
+	/**
+	 * @param nomeGestor the nomeGestor to set
+	 */
+	public void setNomeGestor(String nomeGestor) {
+		this.nomeGestor = nomeGestor;
+	}
+	/**
+	 * @return the telefoneGestor
+	 */
+	public String getTelefoneGestor() {
+		return telefoneGestor;
+	}
+	/**
+	 * @param telefoneGestor the telefoneGestor to set
+	 */
+	public void setTelefoneGestor(String telefoneGestor) {
+		this.telefoneGestor = telefoneGestor;
+	}
+
+}
