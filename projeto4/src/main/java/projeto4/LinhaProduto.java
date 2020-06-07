1c6a842d321b0bdae38d96ac6062ee2c11f58226
Camilla-Marques <camilla.93marques@gmail.com>
2020-06-07 04:49:10 -0300

    Classe responsável por armazenar as linhas de produto

---
+++ projeto4/src/main/java/projeto4/LinhaProduto.java
@@ -0,0 +1,84 @@
+package projeto4;
+
+public class LinhaProduto {
+	
+	private Long codigo;
+	private String nome;
+	private String categoriaLinhaProduto;
+	private String modelo;
+	private String descricao;
+	private Double preco;
+	/**
+	 * @return the codigo
+	 */
+	public Long getCodigo() {
+		return codigo;
+	}
+	/**
+	 * @param codigo the codigo to set
+	 */
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+	/**
+	 * @return the nome
+	 */
+	public String getNome() {
+		return nome;
+	}
+	/**
+	 * @param nome the nome to set
+	 */
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	/**
+	 * @return the categoriaLinhaProduto
+	 */
+	public String getCategoriaLinhaProduto() {
+		return categoriaLinhaProduto;
+	}
+	/**
+	 * @param categoriaLinhaProduto the categoriaLinhaProduto to set
+	 */
+	public void setCategoriaLinhaProduto(String categoriaLinhaProduto) {
+		this.categoriaLinhaProduto = categoriaLinhaProduto;
+	}
+	/**
+	 * @return the modelo
+	 */
+	public String getModelo() {
+		return modelo;
+	}
+	/**
+	 * @param modelo the modelo to set
+	 */
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	/**
+	 * @return the descricao
+	 */
+	public String getDescricao() {
+		return descricao;
+	}
+	/**
+	 * @param descricao the descricao to set
+	 */
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	/**
+	 * @return the preco
+	 */
+	public Double getPreco() {
+		return preco;
+	}
+	/**
+	 * @param preco the preco to set
+	 */
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+}
