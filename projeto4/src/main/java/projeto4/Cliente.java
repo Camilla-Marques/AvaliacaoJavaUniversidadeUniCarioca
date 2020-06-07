2de773c692942bd9487300293d2f18a82910a6df
Camilla-Marques <camilla.93marques@gmail.com>
2020-06-07 04:46:03 -0300

    Classe responsável pelos dados do cliente

---
+++ projeto4/src/main/java/projeto4/Cliente.java
@@ -0,0 +1,58 @@
+package projeto4;
+
+public class Cliente {
+	
+	private Long cliente;
+	private String nome;
+	private String endereco;
+	private String telefone;
+	/**
+	 * @return the cliente
+	 */
+	public Long getCliente() {
+		return cliente;
+	}
+	/**
+	 * @param cliente the cliente to set
+	 */
+	public void setCliente(Long cliente) {
+		this.cliente = cliente;
+	}
+	/**
+	 * @return the nome
+	 */
+	public String getNome() {
+		return nome;
+	}
+	/**
+	 * @param nome the nome to set
+	 */
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	/**
+	 * @return the endereco
+	 */
+	public String getEndereco() {
+		return endereco;
+	}
+	/**
+	 * @param endereco the endereco to set
+	 */
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	/**
+	 * @return the telefone
+	 */
+	public String getTelefone() {
+		return telefone;
+	}
+	/**
+	 * @param telefone the telefone to set
+	 */
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+}
